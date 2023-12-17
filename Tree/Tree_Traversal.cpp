4b86c2776e7c6b6d63d99f509b5c1ad460ec26a7
MD AHSANUL HOQUE ABIR <113261318+ahsanulhoqueabir@users.noreply.github.com>
2023-12-18 02:46:12 +0600

    tree traversal

---
+++ Tree/Tree_Traversal.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define AHSAN                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preOrder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+void inOrder(struct Node* root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+
+}
+void postOrder(struct Node* root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    AHSAN
+
+    // create tree
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    /* Created tree is look like this
+          1
+         / \
+        2   3
+       / \ / \
+      4  5 6  7
+
+*/
+    preOrder(root);
+    cout << endl;
+    inOrder(root);
+    cout << endl;
+    postOrder(root);
+    return 0;
+}
